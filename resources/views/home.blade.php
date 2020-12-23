3c233fe69ec207567bc3984fddeaf5a5eb0cc847
David Alenoghena <43188489+davidalenoghena@users.noreply.github.com>
2020-12-23 14:11:43 +0100

    bug fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,13 +40,13 @@
   <header id="header" class="fixed-top header-transparent">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo mr-auto"><a href="index.html">ILCR</a></h1>
+      <h1 class="logo mr-auto"><a href="">ILCR</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
-          <li class="active"><a href="{{ route ('blog') }}">Home</a></li>
+          <li class="active"><a href="">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#team">Team</a></li>
           <li><a href="#programmes">Programmes</a></li>
