384587aceee6e04fae1ff43e06f74782d300d8bf
David Alenoghena <43188489+davidalenoghena@users.noreply.github.com>
2020-12-23 11:38:21 +0100

    template baker

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,12 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Dami Dami
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel.com/docs">David</a>
+                    <a href="https://laracasts.com">sdsdd</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/portfolio', function () {
+    return view('portfolio');
 });
