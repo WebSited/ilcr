3df906bac5dfef7444cbb4b0afbbd71631aa809f
David Alenoghena <43188489+davidalenoghena@users.noreply.github.com>
2020-12-26 10:14:35 +0100

    logo

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -2,17 +2,16 @@
 <header id="header" class="fixed-top header-transparent">
   <div class="container d-flex align-items-center">
 
-    <h1 class="logo mr-auto"><a href="">ILCR</a></h1>
+    <!--<h1 class="logo mr-auto"><a href="">ILCR</a></h1> -->
     <!-- Uncomment below if you prefer to use an image logo -->
-    <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+    <a href="index.html" class="logo mr-auto"><img src="{{ asset('img/logo.png') }}" alt="" class="img-fluid"></a>
 
     <nav class="nav-menu d-none d-lg-block">
       <ul>
         <li class="active"><a href="{{ route('home') }}">Home</a></li>
         <li><a href="{{ route('home') }}#about">About Us</a></li>
-        <li><a href="{{ route('home') }}#services">Programmes</a></li>
+        <li><a href="{{ route('home') }}#services">Programmes and Admission</a></li>
         <li><a href="{{ route('home') }}#team">Team</a></li>
-        <li><a href="{{ route('home') }}#admission">Admission</a></li>
         <li class="drop-down"><a href="">Centres</a>
           <ul>
             <li class="drop-down"><a href="#">Centre for Land Governance</a>
